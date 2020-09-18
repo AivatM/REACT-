8855ba36fdfcda0e47fccbffff33cc4cfea09228
Aivat <aivat.mendykulov@gmail.com>
2020-09-18 04:34:44 +0300

    Создали компоненты Header и Technology и запихнули в компоненту App

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import './App.css';
 const App = (props) => {
   return (
     <div className="App">
-      <ul>
+      <Header/>
+      <Technology/>
+    </div>
+  );
+}
+const Header = (props) => {
+  return (
+    <h1>ЗДЕСЬ БУДЕТ ШАПКА САЙТА</h1>
+  );
+}
+const Technology = (props) => {
+  return (
+    <ul>
         <li>html</li>
         <li>css</li>
         <li>js</li>
         <li>react</li>
-      </ul>
-    </div>
+    </ul>
   );
 }
 

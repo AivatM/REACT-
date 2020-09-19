687830c6f03edf04ed5fa398b5084119da1624e7
Aivat <aivat.mendykulov@gmail.com>
2020-09-19 04:53:00 +0300

    урок11 удалили Header и Technology, добавили папку с картинками, сверстали шапку, сайтбар, и часть профиля

--- src/App.js
+++ src/App.js
@@ -1,13 +1,80 @@
 import React from 'react'; //Чтобы заработал JSX
 import './App.css';
-import Header from './Header'; //Подключили к файлу App.js компонетну Header
-import Technology from './Technology';//Подключили к файлу App.js компонетну Technology
+import logo from './images/logo.png' //Подключил логотип 
+import profileBg from './images/profileBg.jpg'
+import profilePhoto from './images/profilePhoto.jpg'
+
 
 const App = (props) => {
   return (
-    <div className="App">
-      <Header/>
-      <Technology/>
+    <div className="app-wrapper">
+
+      <header className="header">
+        <div className="container">
+          <a href="#" className="header__logo-link">
+            <img src={logo} alt="logo" className="header__logo-img"/>
+          </a>
+        </div>
+      </header>
+
+      <div className="container">
+
+        <div className="flex">
+
+          <aside className="sitebar">
+            <nav className="menu">
+              <ul className="menu__list">
+                <li className="menu__item">
+                  <a href="#" className="menu__link">Профиль</a>
+                </li>
+                <li className="menu__item">
+                  <a href="#" className="menu__link">Сообщения</a>
+                </li>
+                <li className="menu__item">
+                  <a href="#" className="menu__link">Новости</a>
+                </li>
+                <li className="menu__item">
+                  <a href="#" className="menu__link">Музыка</a>
+                </li>
+                <li className="menu__item">
+                  <a href="#" className="menu__link">Настройки</a>
+                </li>
+              </ul>
+            </nav>
+          </aside>
+
+          <main className="profile">
+            <picture className="profile__bg-wrapper">
+              <img src={profileBg} className="profile__bg-img" alt="profileBg"/>
+            </picture>
+            <div className="profile__card card">
+              <picture className="card__photo-wrapper">
+                <img src={profilePhoto} className="card__photo-img" alt="profilePhoto"/>
+              </picture>
+              <div className="card__text">
+                <div className="card__name">
+                  Эмилия К. 
+                </div>
+                <div className="card__city">
+                  Актобе 
+                </div>
+                <div className="card__education">
+                  Казансий Федеральный Университет 
+                </div>
+                <a href="#" className="card__web"> Мой сайт</a>
+              </div>
+            </div>
+            <div className="profile__posts posts">
+              <h3>Мои записи</h3>
+              <div className="posts__new">
+                <textarea className="posts__window" placeholder="Добавьте запись, Напишите о чем думаете?"></textarea>
+                <button className="posts__btn">Добавить запись</button>
+              </div>
+
+            </div>
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'; //Чтобы заработал JSX
-
-const Header = (props) => {
-    return (
-      <h1>ЗДЕСЬ БУДЕТ ШАПКА САЙТА</h1>
-    );
-}
-
-export default Header; // Даем разрешение на подключение другим файлам

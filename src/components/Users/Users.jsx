a9851cbcc3226b231ef52b28c85668ff2b0de5b2
Aivat <aivat.mendykulov@gmail.com>
2020-10-28 23:30:08 +0300

    54 сделали вызов пользователей не внутри конструктора а внутри классового метода componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,36 +3,33 @@ import './Users.css';
 import UsersItem from './UsersItem/UsersItem';
 import * as axios from 'axios';
 
-const Users = (props) => {
-    if(props.users.length === 0) {
+class Users extends React.Component {
+    
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
             debugger;
-            props.setUsers(response.data.items);
-        }); 
+            this.props.setUsers(response.data.items);
+        });
     }
-  
-    
-    
 
+    render() {
+        return (
+            <section className="users">
+                <h1 className="users__header">
+                    Пользователи
+                </h1>
+                <div className="users__list">
+                    {this.props.users.map(u =>
+                        <UsersItem id={u.id} photoUrl={u.photos.small} followed={u.followed}
+                            name={u.name} status={u.status}
+                            country={'u.location.country'} city={'u.location.city'}
+                            follow={this.props.follow}
+                            unFollow={this.props.unFollow} />)}
+                </div>
+            </section>
 
-    let usersElements = props.users.map(u => 
-    <UsersItem id={u.id} photoUrl={u.photos.small} followed={u.followed}
-    name={u.name} status={u.status} 
-    country={'u.location.country'} city={'u.location.city'}
-    follow={props.follow}
-    unFollow={props.unFollow} />);
-   
-    return (
-        <section className="users">
-            <h1 className="users__header">
-                Пользователи
-            </h1>
-            <div className="users__list">
-                {usersElements}
-            </div>
-        </section>
-    
-    );
+        );
+    }
 }
 
 export default Users;

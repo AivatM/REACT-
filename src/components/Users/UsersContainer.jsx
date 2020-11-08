dc3bb7a95a9e7261ccc4b7b4cb0743417e0dced5
Aivat <aivat.mendykulov@gmail.com>
2020-11-08 14:53:05 +0300

    тоже к 62  нечаяно недосохранил

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -20,7 +20,7 @@ class UsersAPIComponent extends React.Component {
     onPageChanged = (pageNumber) => {
         this.props.setCurrentPage(pageNumber)
         this.props.setIsFetching(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {withCredentials:true}).then(response => {
             this.props.setIsFetching(false);
             this.props.setUsers(response.data.items);
         });

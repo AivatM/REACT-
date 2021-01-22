fff9668a15b983d9b11e05218b1e87b457e2326b
Aivat <aivat.mendykulov@gmail.com>
2021-01-22 13:37:36 +0300

    74 доработали статус, при помощи componentDidUpdate

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -74,7 +74,6 @@ export const getUserProfile = (userId) => (dispatch) => {
 };
 export const getStatus = (userId) => (dispatch) => {
   profileApi.getStatus(userId).then((response) => {
-    debugger;
     dispatch(setStatus(response.data));
   });
 };
